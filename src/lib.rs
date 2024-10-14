9483f74fa462df4acd029cd4f4745a1135ea5a56
Atturi <blackoutwy4545@gmail.com>
2024-10-14 06:18:12 +0300

    home04 initial commit

---
+++ src/lib.rs
@@ -0,0 +1,281 @@
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
+
+/// Шаблон для умных устройств
+pub trait SmartDevice {
+    fn info(&self) -> String;
+    fn get_room_name(&self) -> String;
+    fn set_room_name(&mut self, name: String);
+}
+
+impl Eq for dyn SmartDevice {}
+impl PartialEq for dyn SmartDevice {
+    fn eq(&self, other: &dyn SmartDevice) -> bool {
+        self.info() == other.info() && self.get_room_name() == other.get_room_name()
+    }
+}
+
+impl Hash for dyn SmartDevice {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.info().hash(state)
+    }
+}
+
+/// Умная розетка
+#[derive(Clone)]
+#[allow(dead_code)]
+pub struct Socket {
+    /// Описание
+    info: String,
+    /// Потребляемая мощность
+    power_consumption: u32,
+    /// Состояние(true - включена)
+    is_active: bool,
+    /// Помещение, в котором находится устройство
+    room: String,
+}
+
+impl Socket {
+    /// Создание розетки
+    pub fn new(info: String, power_consumption: u32, is_active: bool, room: String) -> Socket {
+        Socket {
+            info,
+            power_consumption,
+            is_active,
+            room,
+        }
+    }
+    /// Получить текущую потребляемую мощность
+    fn _get_power_consumption(&self) -> u32 {
+        todo!();
+    }
+    /// Включить розетку
+    fn _switch_on(&mut self) {
+        todo!();
+    }
+    /// Выключить розетку
+    fn _switch_off(&mut self) {
+        todo!();
+    }
+    /// Изменить помещение
+    fn _set_room(&mut self, r: String) {
+        self.room = r;
+    }
+}
+
+impl SmartDevice for Socket {
+    fn info(&self) -> String {
+        self.info.clone()
+    }
+
+    fn get_room_name(&self) -> String {
+        self.room.clone()
+    }
+
+    fn set_room_name(&mut self, name: String) {
+        self.room = name;
+    }
+}
+
+/// Умный термометр
+#[derive(Clone)]
+#[allow(dead_code)]
+pub struct Thermometer {
+    /// Описание термометра
+    info: String,
+    /// Температура
+    temperature: f32,
+    /// Помещение, в котором находится термометр
+    room: String,
+}
+
+impl Thermometer {
+    /// Создание термометра
+    pub fn new(info: String, temperature: f32, room: String) -> Thermometer {
+        Thermometer {
+            info,
+            temperature,
+            room,
+        }
+    }
+    /// Получить текущую температуру
+    pub fn get_temperature(&self) -> f32 {
+        self.temperature
+    }
+}
+
+impl SmartDevice for Thermometer {
+    fn info(&self) -> String {
+        self.info.clone()
+    }
+
+    fn get_room_name(&self) -> String {
+        self.room.clone()
+    }
+
+    fn set_room_name(&mut self, name: String) {
+        self.room = name;
+    }
+}
+
+/// Помещение
+pub struct Room {
+    /// Название помещения
+    pub name: String,
+    /// Множество устройств в помещении
+    pub devices: HashSet<Box<dyn SmartDevice>>,
+}
+
+impl Room {
+    /// Получить список устройств в помещении
+    pub fn get_devices(&self) -> Vec<String> {
+        let result: Vec<String> = self.devices.iter().map(|x| x.info().clone()).collect();
+
+        result
+    }
+
+    pub fn add_device(
+        &mut self,
+        mut device: Box<dyn SmartDevice>,
+        device_origin: &mut dyn SmartDevice,
+    ) {
+        if !self.devices.contains(&device) {
+            device.set_room_name(self.name.clone());
+            self.devices.insert(device);
+            device_origin.set_room_name(self.name.clone());
+        }
+    }
+}
+
+impl Eq for Room {}
+
+impl PartialEq for Room {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name
+    }
+}
+
+impl Hash for Room {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.name.hash(state);
+    }
+}
+
+/// Умный дом
+pub struct House {
+    /// Название
+    pub name: String,
+    /// Помещения
+    rooms: HashSet<Room>,
+}
+
+impl House {
+    /// Инициализация дома
+    pub fn new(name: String) -> Self {
+        House {
+            name,
+            rooms: HashSet::new(),
+        }
+    }
+    /// Получить список названий помещений
+    pub fn get_rooms(&self) -> Vec<String> {
+        let result: Vec<String> = self.rooms.iter().map(|room| room.name.clone()).collect();
+
+        result
+    }
+    /// Добавить помещение
+    pub fn add_room(&mut self, room: Room) {
+        self.rooms.insert(room);
+    }
+    /// Построение отчёта по источнику информации
+    pub fn create_report(&self, info_provider: &dyn DeviceInfoProvider) -> String {
+        let mut report = String::new();
+
+        for device in info_provider.get_devices().iter() {
+            let device_result: String = match self.rooms.get(&Room {
+                name: device.get_room_name(),
+                devices: HashSet::new(),
+            }) {
+                Some(room) => {
+                    let dvs: HashSet<String> =
+                        HashSet::from_iter(room.devices.iter().map(|d| d.info()));
+                    match dvs.get(&(*device).info()) {
+                        //nm.devices.get(&device){
+                        Some(dv) => format!("{} : {}\n", room.name, dv),
+                        None => format!(
+                            "Ошибка! Устройство {} не найдено в помещении {}\n",
+                            (*device).info(),
+                            (*device).get_room_name()
+                        ),
+                    }
+                }
+                None => format!("Ошибка! Помещение {} не найдено\n", device.get_room_name()),
+            };
+
+            report.push_str(&device_result);
+        }
+
+        report
+    }
+}
+
+/// Источник данных для построения отчёта
+pub trait DeviceInfoProvider {
+    fn get_devices(&self) -> Vec<&dyn SmartDevice>;
+}
+
+/// Источник данных для построения отчёта(заимствующий)
+pub struct BorrowingDeviceInfoProvider<'a> {
+    /// Устройства, по которым строится отчёт
+    pub devices: Vec<&'a dyn SmartDevice>,
+}
+
+impl<'a> Default for BorrowingDeviceInfoProvider<'a> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<'a> BorrowingDeviceInfoProvider<'a> {
+    pub fn new() -> BorrowingDeviceInfoProvider<'a> {
+        BorrowingDeviceInfoProvider {
+            devices: Vec::new(),
+        }
+    }
+}
+
+impl<'a> DeviceInfoProvider for BorrowingDeviceInfoProvider<'a> {
+    fn get_devices(&self) -> Vec<&'a dyn SmartDevice> {
+        self.devices.clone()
+    }
+}
+
+pub struct OwningDeviceInfoProvider {
+    pub devices: Vec<Box<dyn SmartDevice>>,
+}
+
+impl OwningDeviceInfoProvider {
+    pub fn new() -> OwningDeviceInfoProvider {
+        OwningDeviceInfoProvider {
+            devices: Vec::new(),
+        }
+    }
+}
+
+impl Default for OwningDeviceInfoProvider {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl DeviceInfoProvider for OwningDeviceInfoProvider {
+    fn get_devices(&self) -> Vec<&dyn SmartDevice> {
+        let mut result_vec: Vec<&dyn SmartDevice> = Vec::new();
+
+        for device in self.devices.iter() {
+            result_vec.push(&(**device));
+        }
+
+        result_vec
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,92 @@
+use SmartHouse::{
+    BorrowingDeviceInfoProvider, House, OwningDeviceInfoProvider, Room, SmartDevice, Socket,
+    Thermometer,
+};
+use std::collections::HashSet;
+
+fn main() {
+    // Тестирование розеток
+    println!("\x1b[93mТестирование розеток:\x1b[0m\n");
+    let mut socket1 = Socket::new(String::from("socket1"), 0, false, "".to_string());
+    let mut socket2 = Socket::new(String::from("socket2"), 1, true, "".to_string());
+
+    let mut room_socket = Room {
+        name: "room_socket".to_string(),
+        devices: HashSet::new(),
+    };
+
+    room_socket.add_device(Box::new(socket1.clone()), &mut socket1);
+    room_socket.add_device(Box::new(socket1.clone()), &mut socket1);
+    room_socket.add_device(Box::new(socket2.clone()), &mut socket2);
+
+    println!("\nsocket1's room: {}", socket1.get_room_name());
+    println!("socket2's room: {}", socket2.get_room_name());
+
+    println!("\nУстройства помещения {}:", room_socket.name);
+    for name in room_socket.get_devices().iter() {
+        println!("{name}");
+    }
+
+    // Тестирование термометров
+    println!("\n\x1b[93mТестирование термометров:\x1b[0m\n");
+    let mut thermometer1 =
+        Thermometer::new("thermometer1".to_string(), -11.3_f32, "garden".to_string());
+
+    let mut room_thermometer = Room {
+        name: "room_thermometer".to_string(),
+        devices: HashSet::new(),
+    };
+
+    room_thermometer.add_device(Box::new(thermometer1.clone()), &mut thermometer1);
+
+    println!("\nthermometer1's room: {}", thermometer1.get_room_name());
+
+    println!("\nУстройства помещения {}:", room_thermometer.name);
+    for name in room_thermometer.get_devices().iter() {
+        println!("{name}");
+    }
+
+    //Тестирование дома
+    println!("\n\x1b[93mТестирование дома:\x1b[0m\n");
+    let mut house = House::new("house1".to_string());
+
+    println!("Помещения дома {}:", house.name);
+    for room in house.get_rooms().iter() {
+        println!("{}", room);
+    }
+
+    house.add_room(room_socket);
+    house.add_room(room_thermometer);
+
+    println!("\nПомещения дома {}:", house.name);
+    for room in house.get_rooms().iter() {
+        println!("{}", room);
+    }
+
+    println!("\n\x1b[93mСоздание отчётов:\x1b[0m\n");
+
+    let mut bdip = BorrowingDeviceInfoProvider::new();
+    bdip.devices.push(&socket1);
+    bdip.devices.push(&socket2);
+
+    println!("Отчёт 1(borrowing):");
+    println!("{}\n", house.create_report(&bdip));
+
+    let mut odip = OwningDeviceInfoProvider::new();
+
+    let socket3 = Socket::new(
+        "socket3".to_string(),
+        4,
+        true,
+        "not_existsting_room".to_string(),
+    );
+    let socket4 = Socket::new("socket4".to_string(), 5, true, "room_socket".to_string());
+
+    odip.devices.push(Box::new(socket1));
+    odip.devices.push(Box::new(socket3));
+    odip.devices.push(Box::new(thermometer1));
+    odip.devices.push(Box::new(socket4));
+
+    println!("Отчёт 2(owning):");
+    println!("{}\n", house.create_report(&odip));
+}
