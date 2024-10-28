c8601c37fb3a983e5aec0b18c20daca01e5aeec7
Atturi <blackoutwy4545@gmail.com>
2024-10-28 07:42:58 +0300

    added unit tests for Socket

--- src/unit_tests.rs
+++ src/unit_tests.rs
@@ -1,10 +1,15 @@
+#[allow(unused_imports)]
 use crate::Thermometer::Thermometer;
+#[allow(unused_imports)]
 use crate::SmartDevice::SmartDevice;
+#[allow(unused_imports)]
+use crate::Socket::Socket;
 
 #[cfg(test)]
 pub mod test_thermometer
 {
     use super::{Thermometer, SmartDevice};
+
     #[test]
     pub fn get_init_description()
     {
@@ -23,5 +28,44 @@ pub mod test_thermometer
         thermometer.set_room_name("room2".to_string());
 
         assert_eq!("room2".to_string(), thermometer.get_room_name());
+    }
+}
+
+#[cfg(test)]
+pub mod test_socket
+{
+    use super::{Socket, SmartDevice};
+
+    #[test]
+    pub fn get_init_description()
+    {
+        let socket = Socket::new("socket".to_string(), 3, true, "room".to_string());
+
+        assert_eq!(3, socket.get_power_consumption());
+        assert_eq!(true, socket.is_active());
+        assert_eq!("socket".to_string(), socket.info());
+        assert_eq!("room".to_string(), socket.get_room_name());
+    }
+
+    #[test]
+    pub fn switch_on_off()
+    {
+        let mut socket = Socket::new("socket".to_string(), 3, false, "room".to_string());
+
+        socket.switch_on();
+        assert_eq!(true, socket.is_active());
+
+        socket.switch_off();
+        assert_eq!(false, socket.is_active());
+    }
+
+    #[test]
+    pub fn set_room_name()
+    {
+        let mut socket = Socket::new("socket".to_string(), 3, false, "room1".to_string());
+
+        socket.set_room_name("room2".to_string());
+
+        assert_eq!("room2".to_string(), socket.get_room_name());
     }
 }
