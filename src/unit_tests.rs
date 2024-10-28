9ff56a2074966aff73780539f66014e1086f5026
Atturi <blackoutwy4545@gmail.com>
2024-10-28 06:27:39 +0300

    added SmartDevice unit tests for Thermometer

--- src/unit_tests.rs
+++ src/unit_tests.rs
@@ -1,14 +1,27 @@
 use crate::Thermometer::Thermometer;
+use crate::SmartDevice::SmartDevice;
 
 #[cfg(test)]
 pub mod test_thermometer
 {
-    use super::Thermometer;
+    use super::{Thermometer, SmartDevice};
     #[test]
-    pub fn get_correct_temperature()
+    pub fn get_init_description()
     {
         let thermometer = Thermometer::new("thermometer".to_string(), 19.3, "room".to_string());
 
         assert_eq!(19.3, thermometer.get_temperature());
+        assert_eq!("thermometer".to_string(), thermometer.info());
+        assert_eq!("room".to_string(), thermometer.get_room_name());
+    }
+
+    #[test]
+    pub fn set_room_name()
+    {
+        let mut thermometer = Thermometer::new("thermometer".to_string(), 19.3, "room1".to_string());
+
+        thermometer.set_room_name("room2".to_string());
+
+        assert_eq!("room2".to_string(), thermometer.get_room_name());
     }
 }
